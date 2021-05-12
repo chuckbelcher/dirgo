936dfe78f0788c1f83373f903e2b89187c13c636
Chuck Belcher <cfbelcher@pateogroup.com>
2021-05-12 14:40:28 -0400

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Coming Soon ...</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
