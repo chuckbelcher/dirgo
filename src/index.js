4200a0072a8a3cc194d8fbc5c611c88edf1664dd
Chuck Belcher <cfbelcher@pateogroup.com>
2021-05-12 21:22:22 -0400

    added semantic package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/layout/App';
+import 'semantic-ui-css/semantic.min.css'
 import "./app/layout/App.css";
 //import reportWebVitals from './reportWebVitals';
 
